2aa23e272d04b23aeddda4cf4e8dbc9fadda1493
Aubrey Sherman <sherman.aubrey@gmail.com>
2024-09-12 09:19:08 -0400

    Save token from Auth0 for backend auth - WIP

--- src/BabyApp.tsx
+++ src/BabyApp.tsx
@@ -1,7 +1,26 @@
+import { useState, useEffect } from "react";
+import { jwtDecode as decode } from "jwt-decode";
+
+import BabyBootcampAPI from "./api/api.js";
 import RoutesList from './RoutesList.tsx';
 
+/** Baby Bootcamp application.
+ *
+ * Props: none
+ *
+ * Effect: fetches user's data upon successful login
+ *
+ * Baby Bootcamp -> RoutesList
+ */
 function BabyApp() {
   console.log("* BabyApp");
+  const [currUser, setCurrUser] = useState(null);
+
+  const savedToken = localStorage.getItem('token');
+  BabyBootcampAPI.token = savedToken || '';
+  const [token, setToken] = useState(savedToken || '');
+
+
 
   return (
     <div className="BabyApp">
