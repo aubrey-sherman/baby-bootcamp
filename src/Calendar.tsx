88636808f2c07b807df7faa2ae2c64a00b29cbbe
Aubrey Sherman <sherman.aubrey@gmail.com>
2024-09-12 09:19:50 -0400

    Render Calendar component for daily feed views/updates - WIP

---
+++ src/Calendar.tsx
@@ -0,0 +1,18 @@
+
+/** Calendar component.
+ *
+ * Displayer's the current feeding times for a logged-in user.
+ *
+ * Eat -> Calendar
+ */
+function Calendar() {
+  console.log("* Calendar")
+
+  return (
+    <p>Feeding Calendar</p>
+  )
+}
+
+// TODO: Day component (like Productiv to-do), which can render the update form
+
+export default Calendar;
--- src/Eat.tsx
+++ src/Eat.tsx
@@ -1,8 +1,11 @@
+import Calendar from "./calendar.tsx";
+
 function Eat() {
   console.log("* Eat")
 
   return (
     <p>Eat</p>
+    // Render Calendar component, which has individual calendar cell components
   )
 }
 
