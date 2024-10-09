ca421fc6483d120c2912ebaa14d1e7e8c0be323b
Aubrey Sherman <sherman.aubrey@gmail.com>
2024-10-09 10:38:49 -0400

    Protect routes from unauthorized users

--- src/RoutesList.tsx
+++ src/RoutesList.tsx
@@ -3,6 +3,7 @@ import NavBar from "./NavBar.tsx";
 import Home from "./Home.tsx";
 import Eat from "./Eat.tsx";
 import Sleep from "./Sleep.tsx";
+import { tCurrentUser } from "types.tsx";
 
 /** Routes for Baby App
  *
@@ -14,16 +15,26 @@ import Sleep from "./Sleep.tsx";
  *    {Navigation, LoginForm, RegisterForm, Logout, Home, Eat, Sleep}
  */
 
-function RoutesList() {
+function RoutesList({ currentUser: tCurrentUser }) {
   console.log("* RoutesList");
 
   return (
     <div className="RoutesList">
       <BrowserRouter>
         <Routes>
-          <Route path="/eat" element={<Eat />} />
-          <Route path="/sleep" element={<Sleep />} />
           <Route path="/" element={<Home />} />
+        {/* {currentUser === null &&
+            <>
+              <Route path="/login" element={<LoginForm handleLogin={handleLogin} />} />
+              <Route path="/signup" element={<SignupForm handleSignup={handleSignup}  />} />
+            </>
+          } */}
+          {currentUser !== null &&
+            <>
+            <Route path="/eat" element={<Eat />} />
+            <Route path="/sleep" element={<Sleep />} />
+            </>
+          }
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </BrowserRouter>
