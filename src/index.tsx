c014aa26a5fdeb39dcf392c54b1895ded420f66e
Aubrey Sherman <sherman.aubrey@gmail.com>
2024-10-19 16:30:16 -0400

    Remove last vestiges of Auth0 from codebase

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { Auth0Provider } from '@auth0/auth0-react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App.tsx';
@@ -13,13 +12,5 @@ import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 
 root.render(
-  <Auth0Provider
-    domain="dev-5hwsazsqknzyyvmj.us.auth0.com"
-    clientId="3kOP1dFNkT4PiD1AL8R82s6eQLTSmGT8"
-    authorizationParams={{
-      redirect_uri: window.location.origin
-    }}
-  >
     <App />
-  </Auth0Provider>,
 );
