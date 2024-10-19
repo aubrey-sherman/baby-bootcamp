5da1c9dac98646695027897ec6dc91af92906f15
Aubrey Sherman <sherman.aubrey@gmail.com>
2024-10-19 16:28:51 -0400

    Rename file and distinguish between auth vs. non-auth views

---
+++ src/Navigation.tsx
@@ -0,0 +1,31 @@
+import { Link } from "react-router-dom";
+import { NavigationProps } from "./types.ts";
+
+/** Navigation bar for site that is shown on every page.
+ *
+ * When user is logged in, shows links to main areas of site.
+ *
+ * BabyApp -> Navigation
+ */
+function Navigation({ currentUser, logOut }: NavigationProps) {
+  console.log("* NavBar", currentUser);
+
+  function loggedInNavigation() {
+    return (
+    <>
+      <Link to="/"> Home </Link>
+      <Link to="/eat"> Eat </Link>
+      <Link to="/sleep"> Sleep </Link>
+      <Link to="/logout" onClick={logOut}> Log out</Link>
+    </>
+    );
+  }
+
+  return (
+    <nav className="Navigation">
+      {currentUser && loggedInNavigation()}
+    </nav>
+  );
+}
+
+export default Navigation;
