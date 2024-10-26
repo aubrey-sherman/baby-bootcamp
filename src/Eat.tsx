d2e0f42bdcfbbad83f0d2a4221fcf02ca9ad907c
Aubrey Sherman <sherman.aubrey@gmail.com>
2024-10-26 14:58:54 -0400

    Update documentation to reflect component hierarchy

--- src/Eat.tsx
+++ src/Eat.tsx
@@ -1,13 +1,17 @@
 import CalendarManager from "./CalendarManager.tsx";
 import { HomeProps } from './types.ts';
 
+/** Presentational component that renders calendar.
+ *
+ * Eat -> CalendarManager
+ */
 function Eat({ currentUser }: HomeProps) {
   console.log("* Eat")
 
   return (
     <div className="Eat">
     <p>Feeding Calendar for {currentUser?.babyName}</p>
-    <CalendarManager />
+    <CalendarManager feedingEntries={currentUser?.feedingEntries} />
     </div>
   )
 }
