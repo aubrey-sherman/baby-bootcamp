0a7e39d117601160f136f8c16d38139c2573eef8
Aubrey Sherman <sherman.aubrey@gmail.com>
2024-10-26 14:58:30 -0400

    Render child components - WIP

--- src/CalendarCell.tsx
+++ src/CalendarCell.tsx
@@ -1,17 +1,34 @@
+import { useState } from 'react';
+import FeedingTimeCell from './FeedingTimeCell';
+import FeedingAmountCell from './FeedingAmountCell';
+import { FeedingEntry } from './types';
+
+type CalendarCellProps = {
+  eventTime?: FeedingEntry['eventTime'];
+}
 
 /** Calendar cell component.
  *
  * Displays time and feeding amount (in ounces) for a given day and time block.
  *
- * Eat -> Calendar -> CalendarCell
+ * CalendarView -> CalendarCell -> { TimeCell, AmountCell }
  */
-function CalendarCell() {
+function CalendarCell({ eventTime }: CalendarCellProps) {
+  const [time, setTime] = useState<string>(eventTime);
   console.log("* Calendar")
+
+  /** Sends updated time to backend. */
+  function onTimeSave(newTime: string): void {
+    console.log(`Time is being sent to server as ${newTime}!`);
+  }
 
   return (
     <div className="CalendarCell">
-    <p>Time: </p>
-    <p>Amount: </p>
+      <FeedingTimeCell
+        onTimeSave={onTimeSave}
+        initialTime={feedingEntry.eventTime}
+      />
+      <FeedingAmountCell />
     </div>
   )
 }
