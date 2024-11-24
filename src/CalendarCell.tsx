43ea9343d1f2fc3855b3ee2c1c3042311addea10
Aubrey Sherman <sherman.aubrey@gmail.com>
2024-11-24 11:49:50 -0500

    Fix date type to correctly display in form

--- src/CalendarCell.tsx
+++ src/CalendarCell.tsx
@@ -1,39 +1,72 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { DateTime } from 'luxon';
 import FeedingTimeCell from './FeedingTimeCell';
-import FeedingAmountCell from './FeedingAmountCell';
 import { FeedingEntry } from './types';
-import { formatDateToTimeString } from './helpers/utils';
+import FeedingAmountCell from './FeedingAmountCell';
+import TimezoneHandler from './helpers/TimezoneHandler';
 import './CalendarCell.css';
 
 type CalendarCellProps = {
-  feedingEntries: FeedingEntry[];
-  date: number;
+  feedingEntry?: FeedingEntry | undefined;
+  currentDate: DateTime;
+  onTimeSave: (blockId: string, entryId: string, newTime: DateTime) => void;
+  onAmountSave: (blockId: string, entryId: string, newAmount: number) => void;
 }
 
-/** Calendar cell component.
+/** Calendar cell component displays time and feeding amount (in ounces) for a
+ * given day and time block.
  *
- * Displays time and feeding amount (in ounces) for a given day and time block.
+ * Props:
+ * - feedingEntry
+ * - date: DateTime object representing the day
+ * - currentDate: DateTime object maintained by CalendarManager
  *
- * CalendarView -> CalendarCell -> { TimeCell, AmountCell }
+ * State: none
+ *
+ * Renders:
+ * - If feedingEntry exist: displays time for each entry
+ * - If no entry: displays time form with currentDate from parent
+ *
+ * CalendarView -> CalendarCell -> { FeedingTimeCell, FeedingAmountCell }
  */
-function CalendarCell({ feedingEntries, date }: CalendarCellProps) {
-
-  function updateTime(newDate: Date) {
-    // TODO: Send to database
-    console.log('Saving new date:', newDate);
-  };
+function CalendarCell({
+  feedingEntry,
+  currentDate,
+  onTimeSave,
+  onAmountSave
+}: CalendarCellProps) {
+  const tzHandler = new TimezoneHandler();
 
   return (
-    <td className="CalendarCell">
-      Today is {date}
-      {feedingEntries.map(entry => (
-          <div key={entry.id} className="feeding-entry">
+    <td className={currentDate.hasSame(DateTime.now(), 'day') ? 'current-day' : ''}>
+      <div>
+        {feedingEntry ? (
+          <div className="feeding-entry">
             <FeedingTimeCell
-              eventTime={entry.eventTime}
-              updateTime={updateTime}
+              feedingTime={tzHandler.parseToUserTimezone(feedingEntry.feedingTime)}
+              onTimeSave={onTimeSave}
+              entryId={feedingEntry.id}
+              blockId={feedingEntry.blockId}
+            />
+            <FeedingAmountCell
+              volumeInOunces={feedingEntry.volumeInOunces ? feedingEntry.volumeInOunces : 0}
+              onAmountSave={onAmountSave}
+              entryId={feedingEntry.id}
+              blockId={feedingEntry.blockId}
             />
           </div>
-    ))}
+        ) : (
+          <div>
+            <FeedingTimeCell
+              feedingTime={undefined}
+              onTimeSave={onTimeSave}
+            />
+            <FeedingAmountCell
+              onAmountSave={onAmountSave}
+            />
+          </div>
+        )}
+      </div>
     </td>
   );
 }
