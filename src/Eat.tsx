d2d580858348d0774df5c869b972a1ed2fef5803
Aubrey Sherman <sherman.aubrey@gmail.com>
2024-11-24 11:40:43 -0500

    Import styling - WIP

--- src/Eat.tsx
+++ src/Eat.tsx
@@ -1,16 +1,19 @@
 import CalendarManager from "./CalendarManager.tsx";
-import { HomeProps } from './types.ts';
+import { EatProps } from './types.ts';
+import './Eat.css';
 
 /** Presentational component that renders calendar.
  *
- * Eat -> CalendarManager
+ * RoutesList -> Eat -> CalendarManager
  */
-function Eat({ currentUser }: HomeProps) {
+function Eat({ username, babyName }: EatProps) {
 
   return (
     <div className="Eat">
-    <p>Feeding Calendar for {currentUser?.babyName}</p>
-    <CalendarManager feedingEntries={currentUser?.feedingEntries} />
+    <p>Feeding Calendar for {babyName}</p>
+    <CalendarManager
+      username={username}
+    />
     </div>
   )
 }
